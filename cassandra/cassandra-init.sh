61cf7b5f4ce1fd594cbb327d39e3cf8b7be0cf77
lenawe <50956240+lenawe@users.noreply.github.com>
2023-12-03 21:16:33 +0100

    WLMU-29 Add creation of tables

--- cassandra/cassandra-init.sh
+++ cassandra/cassandra-init.sh
@@ -7,3 +7,7 @@ done
 
 echo "Creating keyspace in Cassandra."
 cqlsh cassandra -u cassandra -p cassandra -e "CREATE KEYSPACE IF NOT EXISTS WATER_LEVEL_MONITORING_UK WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};"
+
+echo "Creating tables in Cassandra."
+cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS water_level_monitoring_uk.stations(uuid uuid primary key, rloiid text, label text, measures_id text, notation text, rivername text, typicalrangehigh float, typicalrangelow float, town text, lat float, long float);"
+cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS water_level_monitoring_uk.measures(uuid uuid primary key, id text, stationreference text, datetime text, value float, unit text);"
