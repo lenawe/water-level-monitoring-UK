ecbdb201b83eb57cc5977c97de3379d4902f3f9b
lenawe <50956240+lenawe@users.noreply.github.com>
2023-12-06 13:44:56 +0100

    WLMU-37 Perform query

--- streamlit/data_app.py
+++ streamlit/data_app.py
@@ -1,2 +1,6 @@
 import streamlit as st
+# Set-up connection
 conn = st.connection("postgresql", type="sql")
+
+# Perform query
+df = conn.query('SELECT * FROM water_level_monitoring_uk.stationsmeasurements;', ttl="2s")
