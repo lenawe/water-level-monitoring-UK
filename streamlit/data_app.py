6f98d73c947c997c75bdc755cc78a107872b820c
lenawe <50956240+lenawe@users.noreply.github.com>
2023-12-06 13:46:01 +0100

    WLMU-37 Adjust date format

--- streamlit/data_app.py
+++ streamlit/data_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import pandas as pd
 # Set-up connection
 conn = st.connection("postgresql", type="sql")
 
 # Perform query
 df = conn.query('SELECT * FROM water_level_monitoring_uk.stationsmeasurements;', ttl="2s")
+
+# Adjust date format
+df['last_update'] = pd.to_datetime(df['last_update'])
+df['last_update'] = df['last_update'].dt.tz_convert('Europe/Vienna')
+df['last_update'] = df['last_update'].dt.strftime('%Y-%m-%d %H:%M')
