199279fdd029efc74bdee0f74ee1e3496c337320
lenawe <50956240+lenawe@users.noreply.github.com>
2023-12-06 08:45:28 +0100

    WLMU-33 Set-up service for PostgreSQL processing

---
+++ spark-consumer-postgresql/spark-setup.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+cd /home/scripts
+
+spark-submit --packages org.apache.kafka:kafka-clients:3.4.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0,org.apache.spark:spark-streaming-kafka-0-10-assembly_2.12:3.3.0,org.apache.commons:commons-pool2:2.11.1,com.datastax.spark:spark-cassandra-connector_2.12:3.3.0,com.twitter:jsr166e:1.1.0,com.datastax.spark:spark-cassandra-connector-assembly_2.12:3.3.0,org.postgresql:postgresql:42.7.0 \
+stations-postgresql.py
