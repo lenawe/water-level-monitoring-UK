e3b0bd1413c62c8c2b4d94f116c871887ce0b912
lenawe <50956240+lenawe@users.noreply.github.com>
2023-12-06 08:54:39 +0100

    WLMU-33 Save to postgres

--- spark-consumer-postgresql/scripts/stations-postgresql.py
+++ spark-consumer-postgresql/scripts/stations-postgresql.py
@@ -50,3 +50,27 @@ def get_expanded_df(topic):
       .selectExpr("CAST(value AS STRING)") \
       .select(from_json(col("value"),get_schema(topic)).alias(topic)) \
       .select("{topic}.*".format(topic=topic))
+
+def save_to_postgres(writeDF, epoch_id, topic):
+  db_credentials = {
+    "user": "postgres",
+    "password": "postgres",
+    "driver" : "org.postgresql.Driver"
+  }  
+  
+  writeDF.write \
+    .jdbc(
+      url="jdbc:postgresql://postgresql:5432/WATER_LEVEL_MONITORING_DB",
+      table="WATER_LEVEL_MONITORING_UK.{topic}".format(topic=topic),
+      mode="append",
+      properties=db_credentials
+    )
+  
+  print(epoch_id, ": Saved messages from topic {topic} PostgreSQL.".format(topic=topic))
+
+def save_to_postgres_stations(writeDF, epoch_id):
+  save_to_postgres(writeDF, epoch_id, "stations")
+
+def save_to_postgres_measurements(writeDF, epoch_id):
+  save_to_postgres(writeDF, epoch_id, "measurements")
+
