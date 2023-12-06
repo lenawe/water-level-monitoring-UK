fe6864858ba72c65d5941f5eb0f03f33dc8b2941
lenawe <50956240+lenawe@users.noreply.github.com>
2023-12-06 13:48:51 +0100

    WLMU-37 Round numerical values

--- streamlit/data_app.py
+++ streamlit/data_app.py
@@ -13,3 +13,7 @@ df['last_update'] = df['last_update'].dt.strftime('%Y-%m-%d %H:%M')
 
 # Adjust unit
 df['unit'] = df['unit'].replace('http://qudt.org/1.1/vocab/unit#Meter', 'm')
+
+# Round numerical values
+df['typical_range_high'] = df['typical_range_high'].round(2)
+df['typical_range_low'] = df['typical_range_low'].round(2)
